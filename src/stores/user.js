c8ee4703b2c5daa992b5abd3c5b7d6a501bad75a
huyiling1111 <1052859577@qq.com>
2021-03-04 00:53:31 +0800

    新增一个user store来存放用户的信息

---
+++ src/stores/user.js
@@ -0,0 +1,17 @@
+import { observable, action } from 'mobx';
+import { Auth } from '../models';
+
+class UserStore {
+    @observable currentUser = null;
+
+    @action pullUser() {
+        this.currentUser = Auth.getCurrentUser()
+    }
+
+    @action resetUser() {
+        this.currentUser = null;
+    }
+}
+
+
+export default new UserStore() ;
