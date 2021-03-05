3a57d7130852305decfce1b2b35585bbecd15587
huyiling1111 <1052859577@qq.com>
2021-03-05 20:12:22 +0800

    添加历史状态管理

--- src/model/index.js
+++ src/model/index.js
@@ -43,9 +43,22 @@ const Uploader = {
         return new Promise((resolve, reject) => {
             item.save().then(serverFile => resolve(serverFile), error => reject(error));
         });
+    },
+    find({page=0, limit=10}) {
+        const query = new AV.Query('Image');
+        query.include('owner');
+        query.limit(limit);
+        query.skip(page*limit);
+        query.descending('createdAt');
+        query.equalTo('owner', AV.User.current());
+        return new Promise((resolve, reject) => {
+            query.find()
+                .then(results => resolve(results))
+                .catch(error => reject(error))
+        });
     }
 }
-console.log('当前用户',Auth.getCurrentUser())
+Window.Uploader=Uploader
 
 export {
     Auth,Uploader
---
+++ src/stores/history.js
@@ -0,0 +1,37 @@
+import { observable, action } from 'mobx';
+import { Uploader } from '../models';
+import { message } from 'antd';
+
+
+class HistoryStore {
+    @observable list = [];
+    @observable isLoading = false;
+    @observable hasMore = true;
+    @observable page = 0;
+    limit = 10;
+
+    @action append(newList) {
+        this.list = this.list.concat(newList);
+    }
+    @action find() {
+        this.isLoading = true;
+        Uploader.find({page: this.page, limit: this.limit})
+            .then(newList => {
+                this.append(newList);
+                this.page++;
+                if(newList.length < this.limit) {
+                    this.hasMore = false;
+                }
+            }).catch(error => {
+            message.error('加载数据失败');
+        }).finally(() => {
+            this.isLoading = false;
+        });
+    }
+    @action reset() {
+        this.list = [];
+        this.isLoading = false;
+        this.hasMore = true;
+        this.page = 0;
+    }
+}
